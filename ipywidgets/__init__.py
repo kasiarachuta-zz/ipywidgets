a8087da2ea9b4b89098679853a0f12a0bb0bfe27
mgymrek <mgymrek@mit.edu>
2014-01-12 11:47:19 -0500

    including static build tables in install

--- ipywidgets/__init__.py
+++ ipywidgets/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.1"
 
-from .interact import StaticInteract, StaticBuildFigure
+from .interact import StaticInteract, StaticBuildTable, StaticBuildFigure
 from .widgets import RadioWidget, RangeWidget
 from .pretty_table import TableStyle, CellStyle, PrettyTable
